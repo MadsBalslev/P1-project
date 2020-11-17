eae0e38c87d7367029ceff59d102bbfa22ec757c
Casper Ståhl <cstahl20@student.aau.dk>
2020-11-17 14:28:03 +0100

    Jeg har tilføjet: .gitignore fil og i main.c en struct for events: struct event.

--- core/src/main.c
+++ core/src/main.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef struct tm tm;
+
+typedef struct event {
+  char eventName[42];
+  char description[280];
+  tm startTime;
+  tm endTime;
+  char tags [100][3];
+} event;
+
 int main(void) {
   return EXIT_SUCCESS;
 }
