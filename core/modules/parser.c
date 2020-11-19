9c3b778c3e5bd8d2a9d13d59aa7a4827dc27df0f
Mads Balslev <madspbalslev@gmail.com>
2020-11-19 12:27:00 +0100

    Parser kan nu udtrække hele events fra fil

--- core/modules/parser.c
+++ core/modules/parser.c
@@ -7,21 +7,30 @@
 int parse_file(char *file_path) {
   FILE *fp; /* Declare en pointer til en FILE */
   char input[LINE_LEN];
-  int line = 0;
+  int line = 0, isEvent, numOfEvents = 0;
 
   fp = fopen(file_path, "r"); /* Åbner den angivne fil i reading mode */
-  if (fp == NULL) /* Hvis filen ikke findes / eller ikke kan åbnes */
+  if (fp == NULL)             /* Hvis filen ikke findes / eller ikke kan åbnes */
     return EXIT_FAILURE;
 
   while (fgets(input, LINE_LEN, fp)) {
     line++;
 
-    if(strstr(input, "DTSTART")) { /* Find linjer hvor der er DTSTART */
+    if (strstr(input, "BEGIN:VEVENT")) { /* Find linjer hvor der er DTSTART */
+      isEvent = 1;
+      numOfEvents++;
+    }
+
+    if (isEvent) {
       printf("%d: %s", line, input);
+    }
+
+    if (strstr(input, "END:VEVENT")) {
+      isEvent = 0;
     }
   }
 
-  printf("\n");
+  printf("\n\n%d events in total\n\n", numOfEvents);
 
   fclose(fp); /* Luk filen igen */
 
