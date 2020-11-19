dffa7b9a8b458801cc4f7bb9277a31f759a455f8
Mads Balslev <madspbalslev@gmail.com>
2020-11-19 09:51:48 +0100

    Add parser as module

--- core/src/main.c
+++ core/src/main.c
@@ -2,8 +2,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include "calendar.h"
+#include "../modules/parser.h"
 
 int main(void) {
+
+    parse_file("/home/mads/dev/P1/test.ics");
 
     return EXIT_SUCCESS;
 }
