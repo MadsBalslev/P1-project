2e50b258f2430d04c3b01da77475f501f50bfa2e
Taoshi <nicolaikh@live.dk>
2020-11-24 14:33:03 +0100

    Exit failiure hvis programmet fejler med at parse en .ics fil (#7)
    * Exit failiure hvis programmet fejler med at parse en .ics fil
    
    * Fix formatering

--- core/src/main.c
+++ core/src/main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #include "../modules/calendar.h"
 #include "../modules/parser.h"
 
 int main(void) {
-
-    parse_file("test.ics"); /* Path relative from parser.o location */
+    
+    int parse_success = parse_file("test.ics"); /* Path relative from parser.o location */
+    if (parse_success == EXIT_FAILURE) {
+        printf("Failed to parse file test.ics");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
