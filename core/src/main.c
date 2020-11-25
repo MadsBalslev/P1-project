dd4e71da6d060f3e1af54768a10e552bc38df13a
Casper Ståhl <cstahl20@student.aau.dk>
2020-11-25 21:51:12 +0100

    main.c abstraheret ydereligere, nu er der stort set kun funktionskald i main.

--- core/src/main.c
+++ core/src/main.c
@@ -5,13 +5,53 @@
 #include "../modules/calendar.h"
 #include "../modules/parser.h"
 
+/* these should not be void xxx(void) functions */
+void getSearchParameters(void);
+void getCalenderSuite(void);
+int  findAvailableDatesByLooking(void);
+void findAvailableDatesByRestructuring(void);
+void userOutput(void);
+
 int main(void) {
+    int foundDatesByLooking = 0;
+
+    getSearchParameters();
+    getCalenderSuite();
+    foundDatesByLooking = findAvailableDatesByLooking();
+
+    if (foundDatesByLooking) {
+        findAvailableDatesByRestructuring();
+    }
     
-    int parse_success = parse_file("test.ics"); /* Path relative from parser.o location */
+    userOutput();
+
+    /* This should be abstracted further */
+    /* Path relative from parser.o location */
+    /* int parse_success = parse_file("test.ics");
     if (parse_success == EXIT_FAILURE) {
         printf("Failed to parse file test.ics");
         return EXIT_FAILURE;
-    }
+    } */
 
     return EXIT_SUCCESS;
+}
+
+void getSearchParameters(void) {
+
+}
+
+void getCalenderSuite(void) {
+
+}
+
+int  findAvailableDatesByLooking(void) {
+    return 1;
+}
+
+void findAvailableDatesByRestructuring(void) {
+
+}
+
+void userOutput(void) {
+    
 }
