13f64c023f6ef1df32cc11b8b6aad2cc310b26a5
Casper Ståhl <cstahl20@student.aau.dk>
2020-11-26 12:19:09 +0100

    added doc directory for doxygen documentation, also added doxygen documentation: html & latex

---
+++ doc/html/search/defines_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['line_5flen_40',['LINE_LEN',['../parser_8c.html#a45db13d622cb21897aa934e5ad411c83',1,'parser.c']]]
+];
---
+++ doc/html/search/variables_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tags_37',['tags',['../structevent.html#a189241cfd7d3a82116641b0a5a6c2e71',1,'event']]]
+];
