f2df9d15d09e01e1054f9c4733c4ead20f7817bd
Casper Ståhl <cstahl20@student.aau.dk>
2020-11-26 12:35:07 +0100

    change dir doc to docs, added cooltext.gif to docs :)

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['calendar_2eh_0',['calendar.h',['../calendar_8h.html',1,'']]],
+  ['checkargs_1',['checkArgs',['../main_8c.html#a14208cec45d32377e62412b99635d245',1,'main.c']]]
+];
---
+++ docs/html/search/typedefs_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tm_39',['tm',['../calendar_8h.html#a110bf4e13553ccfd709f41c6e92f0ca2',1,'calendar.h']]]
+];
