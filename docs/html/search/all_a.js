8e975bfbbeecd413ce4a3a7a371f95201ab9b7cd
Casper Ståhl <cstahl20@student.aau.dk>
2020-11-26 12:29:38 +0100

    changed dir doc to docs added cooltext...gif

---
+++ docs/html/search/all_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['tags_17',['tags',['../structevent.html#a189241cfd7d3a82116641b0a5a6c2e71',1,'event']]],
+  ['tm_18',['tm',['../calendar_8h.html#a110bf4e13553ccfd709f41c6e92f0ca2',1,'calendar.h']]]
+];
---
+++ docs/html/search/variables_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['description_33',['description',['../structevent.html#a0b1aabde9d5ea5112bb976e6abac5333',1,'event']]]
+];
