0cc0db3dc5cd918bc82179b32503b8b97429b497
Mads Balslev <madspbalslev@gmail.com>
2020-11-26 08:52:00 +0100

    Tag fil som command line argument

--- core/src/main.c
+++ core/src/main.c
@@ -6,52 +6,61 @@
 #include "../modules/parser.h"
 
 /* these should not be void xxx(void) functions */
+void checkArgs(int);
 void getSearchParameters(void);
 void getCalenderSuite(void);
-int  findAvailableDatesByLooking(void);
+int findAvailableDatesByLooking(void);
 void findAvailableDatesByRestructuring(void);
 void userOutput(void);
 
-int main(void) {
-    int foundDatesByLooking = 0;
+int main(int argc, char *argv[]) {
+  int foundDatesByLooking = 0;
+  char *filepath;
 
-    getSearchParameters();
-    getCalenderSuite();
-    foundDatesByLooking = findAvailableDatesByLooking();
+  /* Få fil som commandline argument */
+  checkArgs(argc);
+  filepath = argv[2];
 
-    if (foundDatesByLooking) {
-        findAvailableDatesByRestructuring();
-    }
-    
-    userOutput();
+  getSearchParameters();
+  getCalenderSuite();
+  foundDatesByLooking = findAvailableDatesByLooking();
 
-    /* This should be abstracted further */
-    /* Path relative from parser.o location */
-    /* int parse_success = parse_file("test.ics");
+  if (foundDatesByLooking) {
+    findAvailableDatesByRestructuring();
+  }
+
+  userOutput();
+
+  /* This should be abstracted further */
+  /* Path relative from parser.o location */
+  /* int parse_success = parse_file(filepath);
     if (parse_success == EXIT_FAILURE) {
-        printf("Failed to parse file test.ics");
+        printf("Failed to parse file %s", filepath);
         return EXIT_FAILURE;
     } */
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
+}
+
+void checkArgs(int argc) {
+  if (argc < 2) {
+    printf("Please provide one or more .ics files\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 void getSearchParameters(void) {
-
 }
 
 void getCalenderSuite(void) {
-
 }
 
-int  findAvailableDatesByLooking(void) {
-    return 1;
+int findAvailableDatesByLooking(void) {
+  return 1;
 }
 
 void findAvailableDatesByRestructuring(void) {
-
 }
 
 void userOutput(void) {
-    
 }
