0a316f4e795c2ed34f25d7116c183e5f16612310
Casper Ståhl <cstahl20@student.aau.dk>
2020-11-26 12:38:35 +0100

    deleted dir doc

--- doc/html/search/all_8.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['parse_5ffile_13',['parse_file',['../parser_8c.html#a32d025c34ccd835053b80e1f6e18d5b1',1,'parse_file(char *file_path):&#160;parser.c'],['../parser_8h.html#af39a727930989babdaae5d8fb5543dc2',1,'parse_file(char *):&#160;parser.c']]],
-  ['parser_2ec_14',['parser.c',['../parser_8c.html',1,'']]],
-  ['parser_2eh_15',['parser.h',['../parser_8h.html',1,'']]]
-];
--- doc/html/search/files_2.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['parser_2ec_23',['parser.c',['../parser_8c.html',1,'']]],
-  ['parser_2eh_24',['parser.h',['../parser_8h.html',1,'']]]
-];
