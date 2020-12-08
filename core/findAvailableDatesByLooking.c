ad94ad2351679712cca455ac9398dd6ba6a0c353
Mads Balslev <madspbalslev@gmail.com>
2020-12-08 13:40:54 +0100

    Opskrevet trin for funktion

--- core/findAvailableDatesByLooking.c
+++ core/findAvailableDatesByLooking.c
@@ -1,5 +1,11 @@
 /*
-* 1. Find alt ledig tid
-* 2. Tjek ok ønskede event ligger inde for ledig tid
-* 3. Placer ønskede event på optimalt tidspunkt
+* 1. Sammenflet alle overlappende events til ét samlet event
+* 2. Find ledig tid mellem sammenflettede events
+*
+* Hvis ikke det lykkedes at finde hultid uden i kalender med alle events:
+* 3. Fjern alle events med lavere prioritet end ønskede event
+* 4. Sammenflet alle overlappende events til ét samlet event
+* 5. Find ledig tid mellem sammenflettede events
+*
+* Output fund til bruger
 */
